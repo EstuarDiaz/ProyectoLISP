67c428aa9ba76fc9ec009676aae8480e89875227
Estuar Diaz <33070207+EstuarDiaz@users.noreply.github.com>
2019-03-08 12:15:16 -0600

    Parse file

--- src/Parse.java
+++ src/Parse.java
@@ -1,5 +1,11 @@
-import java.util.Map;
+import java.util.ArrayList;
 
-public class Parse {
-	
+public class Parse{
+	public Node<String> ParseList(ArrayList<String> lista) {
+		for(int i = 0; i < lista.size(); i++) {
+            
+        }
+		Node<String> root = new Node<String>("A");
+		return root;
+   }
 }
