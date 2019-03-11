3baeff800df4252c6d19093df8356490e275758d
Estuar Diaz <33070207+EstuarDiaz@users.noreply.github.com>
2019-03-10 21:21:38 -0600

    Predicados
    Manejo de predicados: EQUAL, <, >, ATOM y LIST. Nota: LIST crea las listas como un string.

--- src/Evaluate.java
+++ src/Evaluate.java
@@ -34,6 +34,56 @@ public class Evaluate {
 				}
 				return Float.toString(result);
 			}
+			else if(("<>EQUAL").contains(branch.getData())) {
+				// En este caso, se obtiene el primer valor y se compara 
+				// con todos los demas, con el criterio de comparacion dado
+				String result = "true";
+				String compareTo = EvalBranch(leaves.get(0));
+				for(int i = 1; i < leaves.size(); i++) {
+					String data = EvalBranch(leaves.get(i));
+					if(branch.getData().equals("EQUAL")) {
+						if(Float.isNaN(Float.parseFloat(compareTo))) {
+							if(!compareTo.equals(data)) {
+								result = "false";
+							}
+						}
+						else {
+							if(Float.compare(Float.parseFloat(compareTo), Float.parseFloat(data)) != 0) {
+								result = "false";
+							}
+						}
+					}
+					else if(branch.getData().equals("<")) {
+						if(Float.parseFloat(compareTo) >= Float.parseFloat(data)) {
+							result = "false";
+						}
+					}
+					else if(branch.getData().equals(">")) {
+						if(Float.parseFloat(compareTo) <= Float.parseFloat(data)) {
+							result = "false";
+						}
+					}
+				}
+				return result;
+			}
+			else if(branch.getData().equals("ATOM")) {
+				// Devulve true si el objeto es de tipo atomo, false en caso contrario
+				String result = "true";
+				for(int i = 0; i < leaves.size(); i++) {
+					if(leaves.get(i).getChildren().size() > 0) {
+						result = "false";
+					}
+				}
+				return result;
+			}
+			else if(branch.getData().equals("LIST")) {
+				// Permite crear una lista
+				String list = "(";
+				for(int i = 0; i < leaves.size(); i++) {
+					list += EvalBranch(leaves.get(i)) + " ";
+				}
+				return list.substring(0, list.length()-1)+")";
+			}
 			else {
 				return "null";
 			}
