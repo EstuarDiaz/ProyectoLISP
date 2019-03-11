2a19d250cfd8592f2082899be3d998bac418a5e9
Estuar Diaz <33070207+EstuarDiaz@users.noreply.github.com>
2019-03-10 20:07:21 -0600

    Comentarios

--- src/Evaluate.java
+++ src/Evaluate.java
@@ -1,11 +1,19 @@
 import java.util.List;
 public class Evaluate {
+	/**
+	 * Recibe un arbol y lo evalua de manera recursiva
+	 * @param branch El arbol a evaluar
+	 * @return retorna un string con el resultado, "null" si falla
+	 */
 	public static String EvalBranch(Node<String> branch) {
 		if(branch.getChildren().size() == 0) {
 			return branch.getData();
 		}
 		else {
 			List<Node<String>> leaves = branch.getChildren();
+			// En el caso de operaciones aritmeticas, se obtiene el primer valor
+			// y luego se aplica la operacion con el segundo operando, el resultado
+			// se opera con el tercero, el resultado se opera con el cuarto (etc.)
 			if(("+*-/").contains(branch.getData())) {
 				float result = 0;
 				result = Float.parseFloat(EvalBranch(leaves.get(0)));
