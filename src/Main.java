2cfd1e88bb06db0c075a9abc77fe2c37f9ff43e5
Estuar Diaz <33070207+EstuarDiaz@users.noreply.github.com>
2019-03-10 20:03:35 -0600

    Poder ejecutar varias instrucciones, i.e. evaluar varias expresiones

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,15 @@ import java.util.List;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Scanner input = new Scanner(System.in);
-		String respuesta = "( * 4 5 ( - 9 8 ) ( + ( / 16 2 ) 2 ) 7 )";//input.nextLine();
+		
+		String respuesta = "(* 4 5 (-9 8) (+ (/ 16 2) 2) 7)(+ 8 6)";//input.nextLine();
 		List<String> lista = Parse.MakeList(respuesta);
-        Node<String> branch = Parse.MakeBranch(lista);
-        
-		String result = Evaluate.EvalBranch(branch);
-        System.out.println("Resultado: " + result);
 
-        System.out.println("");
-		System.out.println("Arbol: ");
-        branch.printData(1);
-
-        System.out.println("");
-		System.out.println("Lista: ");
-		System.out.println(lista.toString());
+		List<List<String>> instrucciones = Parse.GetIntructions(lista);
+		for(int i = 0; i < instrucciones.size(); i++) {
+			Node<String> branch = Parse.MakeBranch(instrucciones.get(i));
+			String result = Evaluate.EvalBranch(branch);
+	        System.out.println(result);
+		}
 	}
 }
--- src/Parse.java
+++ src/Parse.java
@@ -9,6 +9,11 @@ public class Parse{
 		return root;
 	}
 	
+	/**
+	 * Recibe un string y devuelve una lista de tokens
+	 * @param s El string a convertir
+	 * @return la lista de tokens
+	 */
 	public static List<String> MakeList(String s){
 		List<String> lista = new ArrayList<String>();
 		s = s.replace("(", " ( ");
@@ -26,6 +31,13 @@ public class Parse{
         return lista;
 	}
 	
+	/**
+	 * Devuelve la expresion siguiente, ejemplo:
+	 *  para: (+ 9 (+ 6 7)) 8 9 ) 6 5 ... )
+	 *  devuelve: (+ 9 (+ 6 7))
+	 * @param list lista original que contiene almenos una expresion
+	 * @return La primera expresion completa contenida en la lista original
+	 */
 	public static List<String> GetExpression(List<String> list) {
 		int parentesisCount = 1;
 		int toIndex = list.size();
@@ -43,6 +55,27 @@ public class Parse{
 		return list.subList(0, toIndex+1);
 	}
 	
+	/**
+	 * Convierte una lista con varias expresiones en una lista de expresiones
+	 * que representan una linea o instruccion en el programa
+	 * @param texto El texto del programa
+	 * @return Lista de instrucciones o expresiones
+	 */
+	public static List<List<String>> GetIntructions(List<String> texto){
+		List<List<String>> instrucciones = new ArrayList<List<String>>();
+		for(int i = 0; i < texto.size(); i++) {
+			List<String> linea = GetExpression(texto.subList(i, texto.size()));
+			i += linea.size() - 1;
+			instrucciones.add(linea);
+		}
+		return instrucciones;
+	}
+	
+	/**
+	 * Dada una expresion, crea un arbol de manera recursiva que la representa
+	 * @param lista La lista a evaluar
+	 * @return Un arbol de la clase Nodo
+	 */
 	public static Node<String> MakeBranch(List<String> lista) {
 		Node<String> root = new Node<String>(lista.get(1));
 		for(int i = 2; i < lista.size() - 1; i++) {
